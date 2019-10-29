c8c552ae329a11ddc9234ac5135486d044b89d70
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-10-29 13:00:55 +0300

    [sqa]: add more opperations

--- sqa/index.py
+++ sqa/index.py
@@ -5,7 +5,9 @@ def add(a, b):
     return (np.array(a) + np.array(b)).tolist()
 
 HANDLERS = {
-    '+': add
+    '+': lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
 }
 
 def lambda_handler(event, context):
@@ -18,9 +20,11 @@ def lambda_handler(event, context):
             'body': 'Unknown operator'
         }
 
+    result = func(np.array(body['a']), np.array(body['b'])).tolist()
+
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'result': func(body['a'], body['b'])
+            'result': result
         })
     }
--- sqa/test.py
+++ sqa/test.py
@@ -2,12 +2,12 @@ from index import lambda_handler
 import json
 
 body = {
-    'op': '+',
+    'op': '*',
     'a': [1, 2, 3],
-    'b': [3, 2, 1]
+    'b': 10
 }
 
 event_mock = { 'body': json.dumps(body) }
 
-# -> {'statusCode': 200, 'body': '{"result": [4, 4, 4]}'}
+# -> {'statusCode': 200, 'body': '{"result": [10, 20, 30]}'}
 print(lambda_handler(event_mock, None))
