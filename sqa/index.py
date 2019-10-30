b7720cae2b65a822a23ee30187facc463e0dc375
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-10-30 22:40:44 +0300

    [sqa]: add aws-xray-sdk & layer docker build

--- sqa/index.py
+++ sqa/index.py
@@ -1,30 +1,58 @@
 import json
 import numpy as np
+from aws_xray_sdk.core import xray_recorder
 
+
+@xray_recorder.capture('add')
 def add(a, b):
-    return (np.array(a) + np.array(b)).tolist()
+    return a + b
+
+
+@xray_recorder.capture('sub')
+def sub(a, b):
+    return a - b
+
+
+@xray_recorder.capture('mul')
+def mul(a, b):
+    return a * b
+
 
 HANDLERS = {
-    '+': lambda a, b: a + b,
-    '-': lambda a, b: a - b,
-    '*': lambda a, b: a * b,
+    '+': add,
+    '-': sub,
+    '*': mul,
 }
 
+
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    func = HANDLERS.get(body['op'])
+    try:
+        body = json.loads(event['body']) if type(event['body']) is str else event['body']
+        func = HANDLERS.get(body['op'])
 
-    if not func:
+        if not func:
+            return {
+                'statusCode': 400,
+                'body': 'Unknown operator'
+            }
+
+        with xray_recorder.in_segment('computation') as segment:
+            a = np.array(body['a'])
+            b = np.array(body['b'])
+            result = func(a, b).tolist()
+            segment.put_annotation('size', f'{a.size * b.size}')
+
         return {
-            'statusCode': 400,
-            'body': 'Unknown operator'
+            'statusCode': 200,
+            'body': json.dumps({
+                'result': result
+            })
         }
-
-    result = func(np.array(body['a']), np.array(body['b'])).tolist()
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'result': result
-        })
-    }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': str(e),
+                'input': event['body']
+            })
+        }
--- sqa/readme.md
+++ sqa/readme.md
@@ -10,13 +10,19 @@
 3. Подключить **X-Ray** - разметить код тегами и логировать вызовы
 4. Сделать нагрузочное тестирование - послать миллионы запросов и наблюдать за выполнением. Собрать в удобном для анализа метрики производительности.
 5. Сделать `load balancing` нативными средствами **AWS**.
-6. Повторить нагрузочный тест и проанализироваьт результаты
+6. Повторить нагрузочный тест и проанализировать результаты
 
 ## dev
 
+- `make install`
 - `make test`
-- `make zip`
+- `make build` - build & pack aws layer
 
+> use `drapegnik-Python37-numpy-aws-x-ray-sdk.zip` for `numpy` & `aws-xray-sdk` dependencies
+
+> check out [Creating New AWS Lambda Layer](https://medium.com/@qtangs/creating-new-aws-lambda-layer-for-python-pandas-library-348b126e9f3e) & [Layers Docs](https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html)
+
+### api
 ```bash
 curl -X POST https://fkjl4ua0b7.execute-api.us-east-1.amazonaws.com/default/calculator \
 -H "Content-Type: application/json" \
@@ -29,4 +35,19 @@ curl -X POST https://fkjl4ua0b7.execute-api.us-east-1.amazonaws.com/default/calc
 EOF
 
 # -> {"result": [4, 4, 4]}
+```
+
+### load balancer
+```bash
+curl -X POST http://calculator-load-balancer-315442892.us-east-1.elb.amazonaws.com \
+-H "Content-Type: application/json" \
+-d @- << EOF
+{
+  "op": "*",
+  "a": [1, 2, 3],
+  "b": [3, 2, 1]
+}
+EOF
+
+# -> {"result": [3, 4, 3]}
 ```
